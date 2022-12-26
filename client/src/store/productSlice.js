a105d2659daa5169887c89aafcfc917763cc1926
ProffessorLOGAN <82794943+ProffessorLOGAN@users.noreply.github.com>
2022-12-26 18:51:21 +0530

    react-redux and redux-toolkit package installed

---
+++ client/src/store/productSlice.js
@@ -0,0 +1,49 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+
+export const STATUSES = Object.freeze({
+    IDLE: 'idle',
+    ERROR: 'error',
+    LOADING: 'loading',
+});
+
+const productSlice = createSlice({
+    name: 'product',
+    initialState: {
+        data: [],
+        status: STATUSES.IDLE,
+    },
+    reducers: {
+        // setStatus(state, action) {
+        //     state.data = action.payload;
+        // },
+        // setProducts(state, action) {
+        //     state.data = action.payload;
+        // },
+
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchProducts.pending, (state, action) => {
+                state.status = STATUSES.LOADING;
+            })
+            .addCase(fetchProducts.fulfilled, (state, action) => {
+                state.data = action.payload;
+                state.status = STATUSES.IDLE;
+            })
+            .addCase(fetchProducts.rejected, (state, action) => {
+                state.status = STATUSES.ERROR;
+            });
+    },
+});
+
+export const { setProducts, setStatus } = productSlice.actions;
+export default productSlice.reducer;
+
+//Thunks --inbuilt redux middleware
+
+export const fetchProducts = createAsyncThunk('products/fetch', async () => {
+    const res = await fetch('https://fakestoreapi.com/products');
+    const data = await res.json();
+    return data;
+});
